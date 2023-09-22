99e7046bb04855ee4685b0b6afe1e665b1114e2b
Pepito2024 <021370b@utea.edu.pe>
2023-09-22 12:02:10 -0500

    en proceso

--- lib/main.dart
+++ lib/main.dart
@@ -8,9 +8,9 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Calculadora de IMC',
+      title: 'IMC APP',
       theme: ThemeData(
-        primarySwatch: Colors.orange,
+        primarySwatch: Colors.blue,
       ),
       home: MyHomePage(),
     );
@@ -23,65 +23,83 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  double _altura = 160; // Altura inicial en centímetros
-  double _peso = 60;   // Peso inicial en kilogramos
+  double _altura = 170; // Altura predeterminada en cm
+  double _peso = 70; // Peso predeterminado en kg
+
   double _imc = 0;
+  String _resultado = "";
 
   void _calcularIMC() {
-    if (_altura > 0 && _peso > 0) {
-      setState(() {
-        _imc = _peso / ((_altura / 100) * (_altura / 100));
-      });
-    }
+    setState(() {
+      _imc = _peso / ((_altura / 100) * (_altura / 100));
+
+      if (_imc < 18.5) {
+        _resultado = "Bajo peso";
+      } else if (_imc >= 18.5 && _imc <= 24.9) {
+        _resultado = "Peso normal";
+      } else {
+        _resultado = "Sobrepeso";
+      }
+    });
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Calculadora de IMC'),
+        title: Text('IMC APP'),
       ),
       body: Center(
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
+            children: <Widget>[
+            Text(
+              'Bienvenido selecciona tu altura y tu peso',
+              style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+            ),
+            SizedBox(height: 20),
 
             Text(
-              'Peso (kg): ${_peso.toStringAsFixed(2)}',
-              style: TextStyle(fontSize: 18),
-            ),
-            Slider(
-              value: _peso,
-              min: 30,
-              max: 200,
-              onChanged: (value) {
-                setState(() {
-                  _peso = value;
-                });
-              },
-            ),
-                        Text(
-              'Altura (cm): ${_altura.toStringAsFixed(2)}',
-              style: TextStyle(fontSize: 18),
+              'Selecciona tu altura (cm):',
             ),
             Slider(
               value: _altura,
               min: 100,
-              max: 250,
+              max: 220,
               onChanged: (value) {
                 setState(() {
                   _altura = value;
                 });
               },
             ),
+            Text('Altura: ${_altura.toStringAsFixed(1)} cm'),
+            SizedBox(height: 20),
+            Text(
+              'Selecciona tu peso (kg):',
+            ),
+            Slider(
+              value: _peso,
+              min: 30,
+              max: 150,
+              onChanged: (value) {
+                setState(() {
+                  _peso = value;
+                });
+              },
+            ),
+            Text('Peso: ${_peso.toStringAsFixed(1)} kg'),
             SizedBox(height: 20),
             ElevatedButton(
               onPressed: _calcularIMC,
-              child: Text('Calcular'),
+              child: Text('Calcular IMC'),
             ),
             SizedBox(height: 20),
             Text(
               'Tu IMC es: ${_imc.toStringAsFixed(2)}',
+              style: TextStyle(fontSize: 20),
+            ),
+            Text(
+              'Resultado: $_resultado',
               style: TextStyle(fontSize: 20),
             ),
           ],
